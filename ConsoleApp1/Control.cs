dbd8b2718543f5e433dddc69d5abff8b73462964
LORENZO LEAL MARANHO <lorenzo.lmaranho@senacsp.edu.br>
2024-03-18 20:02:39 -0300

    segundo commit

--- ConsoleApp1/Control.cs
+++ ConsoleApp1/Control.cs
@@ -35,13 +35,21 @@ namespace ExercíciosTI20N
                              "14.Exercício 14\n" +
                              "15.Exercício 15\n" +
                              "16.Exercício 16\n" +
-                             "17 Exercicio 17\n" +   
+                             "17 Exercicio 17\n" +
                              "18 Exercicio 18\n" +
                              "19 Exercicio 19\n" +
                              "20 Exercicio 20\n" +
                              "21 Exercicio 21\n" +
                              "22 Exercicio 22\n" +
-                             "23 Exercicio 23\n") 
+                             "23.Exercício 23\n" +
+                             "24.Exercício 24\n" +
+                             "25.Exercício 25\n" +
+                             "26.Exercício 26\n" +
+                             "27.Exercício 27\n" +
+                             "28.Exercício 28\n" +
+                             "29.Exercício 29\n" +
+                             "30.Exercício 30\n" +
+                             "31.Exercício 31\n");
 
 
 
@@ -49,7 +57,7 @@ namespace ExercíciosTI20N
 
 
 
-
+            ;
         }//Fim do Menu
 
         public void Operacao()
@@ -168,6 +176,107 @@ namespace ExercíciosTI20N
                     default:
                         Console.WriteLine("Código digitado não é valido!");
                         break;
+
+                    case 21:
+                        Console.WriteLine("Informe um número: ");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(modelo.ExercicioVinteUm(num));
+                        break;
+                    case 22:
+                        Console.WriteLine("Informe o primeiro número: ");
+                        int num3 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Informe o segundo número: ");
+                        int num4 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(modelo.ExercicioVinteDois(num3, num4));
+                        break;
+                    case 23:
+                        Console.WriteLine("Informe o primeiro número: ");
+                        int num5 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Informe o segundo número: ");
+                        int num6 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(modelo.ExercicioVinteTres(num5, num6));
+                        break;
+
+                    case 24:
+                        Console.WriteLine("Informe um número: ");
+                        num = Convert.ToInt32(Console.ReadLine());
+                    case 24:
+                        Console.WriteLine("Informe um número: ");
+                        num = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(modelo.ExercicioVinteQuatro(num));
+                        break;
+
+                    case 25:
+                        Console.WriteLine("Informe o primeiro número: ");
+                        int num7 = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Informe o segundo número: ");
+                        int num8 = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(modelo.ExercicioVinteCinco(num7, num8));
+                        break;
+
+                    case 26:
+                        Console.WriteLine("Informe a sua altura: ");
+                        double altura = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine(modelo.ExercicioVinteQuatro(num));
+                        break;
+                        Console.WriteLine("Informe o seu peso: ");
+                        double peso = Convert.ToDouble(Console.ReadLine());
+
+                    case 25:
+                        Console.WriteLine("Informe o primeiro número: ");
+                        int num7 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(modelo.ExercicioVinteSeis(altura, peso));
+                        break;
+
+                    case 27:
+                        Console.WriteLine("Informe quantos anos você tem: ");
+                        int ano = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Informe quantos meses passou do seu aniversário: ");
+                        int mes = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Informe quantos dias passsaram após seu aniversário: ");
+                        int dia = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Informe o segundo número: ");
+                        int num8 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(modelo.ExercicioVinteSete(ano, mes, dia));
+                        break;
+
+                    case 28:
+                        Console.WriteLine("Informe o valor da venda: ");
+                        double vendas = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine(modelo.ExercicioVinteCinco(num7, num8));
+                        break;
+                        Console.WriteLine("Informe suas vendas: ");
+                        double salario = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine(modelo.ExercicioVinteOito(vendas, salario));
+                        break;
+
+                    case 29:
+                        Console.WriteLine(modelo.ExercicioVinteNove());
+                        break;
+
+                    case 30:
+                        Console.WriteLine(modelo.ExercicioTrinta());
+                        break;
+
+                    case 31:
+                        Console.WriteLine(modelo.ExercicioTrintaUm());
+                        break;
+
+                    default:
+                        Console.WriteLine("Código digitado não é valido!");
+                        break;
+                        Console.WriteLine("Código digitado não é valido!");
+                        break;
+
 
                 }//Fim da Operação
             } while (opcao != 0);
--- ConsoleApp1/Model.cs
+++ ConsoleApp1/Model.cs
@@ -266,7 +266,7 @@ namespace ExercíciosTI20N
         //temperatura de celcius  para fahrenheit. A formula  é F = C * 9/5 + 32. 
         public double ExercicioDezenove(double celcius)
         {
-            return celcius * 9 / 5 + 32
+            return celcius * 9 / 5 + 32;
 
         }//fim do metodo
 
@@ -280,8 +280,211 @@ namespace ExercíciosTI20N
 
         }//fim do metodo
 
-        //exercicio 21: faça um programa que peça um numero e imprima o seu quadro
-        //
+    }//Fim do Exercício Vinte Um
+     //Exercício 22: Faça um programa que peça dois números e imprima o maior deles.
+        public int ExercicioVinteDois(int num3, int num4)
+    {
+        if (num3 > num4)
+        {
+            return num3;
+        }//Fim do If
+        else
+        {
+            return num4;
+        }//Fim do Else
+    }//Fim do Exercício Vinte Dois
+     //Exercício 23: Faça um programa que peça dois números e imprima "São Iguais" se os números forem iguais ou imprima "São Diferentes" se forem diferentes.
+    public string ExercicioVinteTres(int num5, int num6)
+    {
+        if (num5 == num6)
+            if (num5 == num6)
+            {
+                return "Os números são iguais";
+            }//Fim do If
+            else
+            {
+                return "Os números são diferentes";
+            }//Fim do Else
+    }//Fim do Exercício Vinte Tres
+
+    //Exerc~icio 24: Faça um programa que peça idade do usuário e imprima se ele é maior de idade ou menor de idade.
+    //Exercício 24: Faça um programa que peça idade do usuário e imprima se ele é maior de idade ou menor de idade.
+    public string ExercicioVinteQuatro(int idade)
+    {
+        if (idade >= 18) ;
+
+        return "Menor de idade"
+
+    } };,99;@@ public string ExercicioVinteCinco(int num7, int num8);
+            }//Fim do Else
+        }//Fim do Exercício Vinte Cinco
+ 
+        //Exercício 26: Faça um programa que peça a altura e o peso de uma pessoa e calcule o IMC. A fórmula é IMC = peso / altura^2.
+        public double ExercicioVinteSeis(double altura, double peso)
+{
+        double IMC = 0;
+        IMC = altura / (Math.Pow(peso, 2));
+        return "O IMC é: " + IMC;
+}//Fim do Exercício Vinte Seis
+
+//Exercício 27: Faça um algorítimo que leia a idade de uma pessoa expressa em anos, meses e dias e escreva a idade apenas em dias. Considerar ano com 365 dias e mês com 30 dias.
+    public int ExercicioVinteSete(int ano, int mes, int dia)
+    {   
+    return ano * 365 + mes * 30 + dia;
+    }//Fim do Exercício Vinte Sete
+
+//Exercício 28: Ler o salário fixo e o valor das vendas efetuadas pelo vendedor de uma empresa. Sabendo-se que ele recebe uma comissão 3% sobre o total das vendas até R$ 1.500,00 mais 5% sobre o que ultrapassar este valor, calcular e escrever o seu salário total
+    public double ExercicioVinteOito(double salarioFixo,Double valorVendas)
+    {
+        double salarioFinal = 0;
+        if (valorVendas <= 1500)
+    }
+    else 
+    {
+        salarioFixo + (valorVendas * 0.03) + salarioFixo
+
+    }
+    else
+    {
+        salarioFinal = (1500 * 0, 03) + ((valorVendas - 1500) * 0.05) + salarioFixo;
+    }
+    return salarioFinal;
+
+}//Fim do Exercício Vinte Oito
+
+//Exercício 29: Ler 10 valores e escrever quantos desses valores lidos são negativos.
+    public void ExercicioVinteNove()
+    {   
+        int num = 0;
+        int cont = 0;
+    }
+    
+    for (int i = 0; i <= 10; i++)
+    {
+        Console.WriteLine("Informe um número: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num < 0)
+        {
+            cont++;
+        }//Fim do If
+    }//Fim da For
+
+    return "A quantidade de números negativos são: " + cont;
+}//Fim do Exercício Vinte Nove
+
+//Exercício 30: Escreva um algoritmo para ler 10 números. Todos os números lidos com valor inferior a 40 devem ser somados. Escreva o valor final da soma efetuada.
+public void ExercicioTrinta()
+{
+    int soma = 0;
+    int num = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine("Informe um número: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num < 40)
+        {
+            soma += num;
+        }//Fim do If
+    }//Fim da For
+
+    Console.WriteLine ("A soma dos valores inferiores a 40 é: " + soma);
+
+}//Fim do Exercício Trinta
+
+//Exercicio 31:Escreva um algoritmo que permita a leitura das notas de uma turma de 20 alunos.Calcular a média da turma.Escrever a média da turma.
+public string ExercicioTrintaUm()
+{
+    int soma = 0;
+    int media = 0;
+    int nota = 0;
+
+    for (int i = 0; i <= 20; i++)
+    {
+        Console.WriteLine("Informe um número: ");
+        nota = Convert.ToInt32(Console.ReadLine());
+
+        soma += nota;
+
+    }//Fim do For
+
+    media = soma / 20;
+
+    return "A média da turma é: " + media;
+
+}//Fim do Exercicio Trinta e Um
+    
+//Exercício 32: Faça um algoritmo que calcule a quantidade de litros de
+        //combustível gasta em uma viagem, utilizando um automóvel
+        //que faz 12Km por litro.Para obter o cálculo, o usuário deve
+        //fornecer o tempo gasto na viagem e a velocidade média
+        //durante ela.Desta forma, será possível obter a distância
+        //percorrida com a fórmula DISTANCIA = TEMPO * VELOCIDADE.
+        //Tendo o valor da distância, basta calcular a quantidade de
+        //litros de combustível utilizada na viagem com a fórmula:
+        //LITROS_USADOS = DISTANCIA / 12.O programa deve
+        //apresentar os valores da velocidade média, tempo gasto na
+        //viagem, a distância percorrida e a quantidade de litros
+        //utilizada na viagem
+
+ //Exercício 41: Faça um algoritmo que leia os valores de A, B, C e em
+        //seguida imprima na tela a soma entre A e B é mostre se a soma é menor que C.
+
+ 
+
+        //Exercício 42: Faça um algoritmo que leia dois valores inteiros A e B,
+        //se os valores de A e B forem iguais, deverá somar os dois valores,
+        //senão deverá multiplicar. Ao final de qualquer um dos cálculos deve-se
+        //atribuir o resultado a uma variável C e imprimir na tela.
+
+ 
+
+        //Exercício 43: Faça um algoritmo que receba um número inteiro e imprima na
+        //tela o seu antecessor e o seu sucessor.
+
+ 
+
+        //Exercício 44: Faça um algoritmo que leia o valor do salário mínimo e o
+        //valor do salário de um usuário, calcule quantos salários mínimos esse 
+        //usuário ganha e imprima na tela o resultado.
+
+ 
+
+        //Exercício 45:  Faça um algoritmo que leia três valores inteiros diferentes e
+        //imprima na tela os valores em ordem decrescente.
+
+ 
+
+        //Exercício 46:Faça um algoritmo que leia quatro notas obtidas por um aluno,
+        //calcule a média das nota obtidas, imprima na tela o nome do aluno e 
+        //se o aluno foi aprovado ou reprovado.
+        //Para o aluno ser considerado aprovado sua média final deve ser maior ou igual a 7.
+
+ 
+
+        //Exercício 47: Faça um algoritmo que receba um valor A e B, e troque o valor de A por B e o
+        //valor de B por A e imprima na tela os valores.
+
+ 
+
+        //Exercício 48: Faça um algoritmo que leia o ano em que uma pessoa nasceu,
+        //imprima na tela quantos anos, meses e dias essa pessoa ja viveu. Leve em 
+        //consideração o ano com 365 dias e o mês com 30 dias. (Ex: 5 anos, 2 meses e 15 dias de vida)
+
+ 
+
+        //Exercício 49: Faça um algoritmo que leia três valores que representam os três lados
+        //de um triângulo e verifique se são válidos, determine se o triângulo é 
+        //equilátero, isósceles ou escaleno.
+
+ 
+
+        //Exercício 50: Faça um algoritmo que calcule a quantidade de litros de combustível
+        //gastos em uma viagem, sabendo que o carro faz 12km com um litro.
+        //Deve-se fornecer ao usuário o tempo que será gasto na viagem a sua velocidade média,
+        //distância percorrida e a quantidade de litros utilizados para fazer a viagem.
+        // Fórmula: distância = tempo x velocidade.  litros usados = distância / 12.
 
 
 
@@ -290,7 +493,6 @@ namespace ExercíciosTI20N
 
 
 
-        //exercicio 22: faça um programa que peça dois numeros e imprima o maior deles 
 
 
 
@@ -298,27 +500,24 @@ namespace ExercíciosTI20N
 
 
 
-        //exercicio 23: faça um programa que peça dois numeros e imprima "São iguais" se 
-        //os inumeros forem iguais ou
-        //imprima "são diferentes" se forem diferentes  
 
 
 
-        //exercicio 24: faça um programa que peça a idade dos usuarios e 
-        //imprima se ele é maior de idade ou menor de idade. 
-        
-
-
-        //exercicio 25: faça um programa que peça dois  numeros e imprima o menor deles
 
 
 
-        //exercicio 26: faça um programa  que peça a altura e o peso  de uma pessoa e 
-        //calcule  o IMC (indice de massa  corporal). A formula é IMC = peso / altura
 
-            
-            
-     
 
+
+
+
+
+
+
+
+
+
+
+       
     }//Fim da Classe
 }//Fim do Projeto
